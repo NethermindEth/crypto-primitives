f99579186be7e23462d49f5079a87e4a1d3f6e03
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-11-28 10:33:48 +0000

    Benchmarks for operations on `*_monty_field` types and improved division (#11)
    1. Moved benchmarks for the arithmetic operations here and duplicated
    them for each type of a field.
    2. Shortened the implementations of `Inv` and `CheckedDiv` for all
    bigint fields and improved performance of the operation.
    
    ---------
    
    Co-authored-by: Alex Abdugafarov <frozenspider@users.noreply.github.com>

--- src/field/crypto_bigint_const_monty.rs
+++ src/field/crypto_bigint_const_monty.rs
@@ -255,6 +255,15 @@ impl<Mod: Params<LIMBS>, const LIMBS: usize> Div<&Self> for ConstMontyField<Mod,
     }
 }
 
+impl<Mod: Params<LIMBS>, const LIMBS: usize> Div for &ConstMontyField<Mod, LIMBS> {
+    type Output = ConstMontyField<Mod, LIMBS>;
+
+    #[inline(always)]
+    fn div(self, rhs: Self) -> Self::Output {
+        self.checked_div(rhs).expect("Division by zero")
+    }
+}
+
 impl<Mod: Params<LIMBS>, const LIMBS: usize> Pow<u32> for ConstMontyField<Mod, LIMBS> {
     type Output = Self;
 
@@ -267,12 +276,7 @@ impl<Mod: Params<LIMBS>, const LIMBS: usize> Inv for ConstMontyField<Mod, LIMBS>
     type Output = Option<Self>;
 
     fn inv(self) -> Self::Output {
-        let result = self.0.invert_vartime();
-        if result.is_some().into() {
-            Some(Self(result.unwrap()))
-        } else {
-            None
-        }
+        Some(Self(Option::from(self.0.invert_vartime())?))
     }
 }
 
@@ -284,13 +288,7 @@ impl<Mod: Params<LIMBS>, const LIMBS: usize> Inv for ConstMontyField<Mod, LIMBS>
 impl<Mod: Params<LIMBS>, const LIMBS: usize> CheckedDiv for ConstMontyField<Mod, LIMBS> {
     #[allow(clippy::arithmetic_side_effects)] // False alert
     fn checked_div(&self, rhs: &Self) -> Option<Self> {
-        let inv = rhs.0.invert();
-        if inv.is_none().into() {
-            return None; // Division by zero
-        }
-        // Safe to unwrap since we checked for None above
-        let inv = inv.unwrap();
-        Some(Self(self.0 * inv))
+        Some(self * rhs.inv()?)
     }
 }
 
